f333178bfcbaf2364efa515d55f56b2fecfb3bf5
Pradeep Chaudhary <mr.pradeepchy61@gmail.com>
2026-03-12 21:46:23 +0530

    Largest element in matrix

---
+++ Arrays/2D_Array/07_find_largest_element_in_matrix.c
@@ -0,0 +1,42 @@
+// Write a program to find the largest element in a matrix.
+
+#include<stdio.h>
+int main(){
+    int i, j, rows, cols;
+    printf("Enter the rows and columns of matrix: ");
+    scanf("%d%d", &rows, &cols);
+    int mat[rows][cols];
+
+    //read elements of matrix
+    for(i=0; i<rows; i++){
+        for(j=0; j<cols; j++){
+            printf("Enter the elements of a matrix: ");
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    // Calculate largest
+    int largest = mat[0][0];
+
+    for(i=0; i<rows; i++){
+        for(j=0; j<cols; j++){
+            if(mat[i][j]>largest){
+                largest = mat[i][j];
+            }
+        }
+    }
+    
+    //print matrix
+    printf("Matrix is: \n");
+    for(i=0; i<rows; i++){
+        for(j=0; j<cols; j++){
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Largest element is: %d",largest);
+
+    return 0;
+    
+}
