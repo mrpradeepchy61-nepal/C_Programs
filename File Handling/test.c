a115056cf1ca0a122d966d1db5bf99f16e5f3e66
Pradeep Chaudhary <mr.pradeepchy61@gmail.com>
2026-05-01 11:51:54 +0530

    updated

--- File Handling/test.c
+++ File Handling/test.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 int main() {
     FILE *fp;
+    char text[200];
+
     fp = fopen("data.txt", "w");
-    fprintf(fp,"Hello broo");
+
+    printf("Enter some text:\n");
+    fgets(text, sizeof(text), stdin);
+    
+    fprintf(fp,"%s", text);
+    
     fclose(fp);
     printf("Data written successfully");
     return 0;
