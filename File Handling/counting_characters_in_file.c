6770cbec11c373f6d1aa31d6e1c112c818155db2
Pradeep Chaudhary <mr.pradeepchy61@gmail.com>
2026-05-01 11:36:13 +0530

    count characters in a file

---
+++ File Handling/counting_characters_in_file.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    FILE *fp = fopen("data.txt", "r");
+    int count = 0;
+    while(fgetc(fp) != EOF){
+        count++;
+    }
+    printf("Characters in file: %d", count);
+}
